8a379316d241e6829f75fd2c3eb254a661ce0e0b
toan0176 <xuantoan2948@gmail.com>
2021-01-01 14:56:04 +0100

    edit app6

---
+++ app6.js
@@ -0,0 +1,18 @@
+ /*App6: json*/
+
+ //define an object
+ let myInfo ={
+    name:'toan',
+    gender:'male' ,
+    city: 'berlin',
+    showname: function(){
+        return this.name ;
+    }
+};
+function showInfo(){
+    // turn an object to a json
+    let j = JSON.stringify(myInfo);
+    // turn an definition (json?) to an object
+    let o =JSON.parse(j);
+   document.getElementById('jsonInfo').innerText= myInfo.showname() + j;
+}
