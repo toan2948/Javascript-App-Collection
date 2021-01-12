667b088bd10970f6b915ad70b1a177fafb78cf0f
toan0176 <xuantoan2948@gmail.com>
2021-01-12 21:07:54 +0100

    edit app-alarm.js, add comments

--- js/app-alarm.js
+++ js/app-alarm.js
@@ -1,25 +1,19 @@
+// erzeugen die Select input für Stunde und Minuten
 function generateSelect(){
   let html ='';
-  html+=  "<select id='select-hour' onchange='selectHour()'>";
+  html+=  "<select id='select-hour'>";
   for(let i=0;i<25;i++){
     html +=  '<option value="'+i +'">'+i+'</option>';
   }           
   html+='</select>' +':';
   document.getElementById('hour').innerHTML=html;
   html='';
-  html+=  "<select id='select-minute' onchange='selectMinute()'>";
+  html+=  "<select id='select-minute'>";
   for(let i =0;i<60;i++){
     html +=  '<option value="'+i +'">'+i+'</option>';
   }           
   html+='</select>';
   document.getElementById('minute').innerHTML=html;
-}
-
-function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
 }
 
 let hour ='';
@@ -30,6 +24,15 @@ let m ='';
 let s='';
 let audio;
 
+//füge 0 vor Minuten, Sekunden falls sie als 10 kleiner sind
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+//zeigen die aktuelle Zeit
 function startTime() {
   let d = new Date();
   h=d.getHours();
@@ -37,11 +40,10 @@ function startTime() {
   s=d.getSeconds();
   m= checkTime(m);
   s= checkTime(s);
-  document.getElementById('time').innerText=
-  h + ':'+m+':'+s;
+  document.getElementById('time').innerText= h + ':'+m+':'+s;
 }
 
-
+//füge ein Change event zu select-hour
 document.addEventListener('change',function(e){
   if(e.target && e.target.id== 'select-hour'){
         hour =document.getElementById('select-hour').value;
@@ -54,20 +56,20 @@ document.addEventListener('change',function(e){
    }
 });
 
+audio = new Audio('alarm.mp3');
 function setAlarm(){
   timeDown = (hour-h)*3600 + (minute-m)*60;
   let timer = setInterval(function(){
       timeDown--;
       document.getElementById('show-timeDown').innerHTML=timeDown + ' :second(s) left'
-      if(timeDown<=0){
-        clearInterval(timer);
+      if(timeDown==0){
+        clearInterval(timer); //stoppt timer
         document.getElementById('show-timeDown').innerHTML='Alarmed!!!';
-        audio = new Audio('alarm.mp3');
         audio.play();
         let btnStop = document.getElementById('stop-alarm');
-        btnStop.style.display='block';
+        btnStop.style.display='block'; //zeigen die 'Stop alarm' Taste
         btnStop.value='Stop Alarm';
-        //add a click to 'stop-alarm' button
+        //füge ein click event zu die 'Stop-alarm' Taste
         btnStop.addEventListener("click", turnoffAlarm, false);
         document.getElementById('set-alarm').style.display='none';
       }
