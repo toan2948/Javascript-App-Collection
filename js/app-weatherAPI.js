b6f7b3f56c73b817ef7f2e1ddb09de199cd2f56c
toan0176 <xuantoan2948@gmail.com>
2021-02-01 17:48:45 +0100

    add icons to the weather app

--- js/app-weatherAPI.js
+++ js/app-weatherAPI.js
@@ -2,6 +2,15 @@ const api_key = '7930eb19227faa28448ec7f56358ee63';
 
 const city_form = document.getElementById('submit_city');
 
+const fetchWeatherData_Berlin = async ()=>{  
+    const baseURL =`https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid=${api_key}&units=metric`;
+    // without 'https://' it does not work
+    const response = await fetch(baseURL); 
+    const data =await response.json();
+    console.log(data);
+    return data;
+}
+
 const fetchWeatherData = async (location)=>{  
         const baseURL =`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${api_key}&units=metric`;
         // without 'https://' it does not work
@@ -13,10 +22,14 @@ const fetchWeatherData = async (location)=>{
 
 const processWeatherData = async (location)=>{
     const data = await fetchWeatherData(location);
+    let icon_id = data.weather[0].icon;
+    const image= `http://openweathermap.org/img/wn/${icon_id}@2x.png`;
     document.getElementById('showWeatherData').innerHTML =`
-    <h1>Temp: ${data.main.temp} °C</h1>
+    <h3 style="color:blue">${data.name}</h3>
+    <p>Temp: ${data.main.temp} °C</p>
     <p>Humidity: ${data.main.humidity} %</p>
     <p>Wind: ${data.wind.speed} km/h</p>
+    <img src=${image} alt=""/>
     `;
 }
 
